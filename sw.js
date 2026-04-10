c5b7cbe729f8143b12aa119c996297753e41daac
ranjith22808 <ranjith22808@gmail.com>
2026-04-10 21:50:19 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = 'ranjith-v1';
+const assets = ['/', '/index.html'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(assets))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
