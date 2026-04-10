d0b6875065a8946f81cc3a1cb8d897f586423345
ranjith22808 <ranjith22808@gmail.com>
2026-04-10 22:14:56 +0530

    For PWA
    updated all codes

--- sw.js
+++ sw.js
@@ -1,18 +1,10 @@
-const CACHE_NAME = 'ranjith-v1';
+const CACHE_NAME = 'ranjith-card-v1';
 const assets = ['./', './index.html', './profile.jpg'];
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(assets);
-    })
-  );
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(assets)));
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
